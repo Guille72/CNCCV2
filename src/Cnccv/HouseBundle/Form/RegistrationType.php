6ebb7fb037763b4607363b24be06ed77bc9ce8b8
jayce <jayce.grandieres@laposte.net>
2017-10-06 17:34:29 +0200

    Fonction register et README à lire

--- src/Cnccv/HouseBundle/Form/RegistrationType.php
+++ src/Cnccv/HouseBundle/Form/RegistrationType.php
@@ -1,8 +1,20 @@
 <?php
 namespace Cnccv\HouseBundle\Form;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
 
 class RegistrationType extends AbstractType
 {
-    public function 
+    public function buildForm(FormBuilderInterface $builder, array $option)
+    {
+        $builder
+            ->add('firstname', TextType::class)
+            ->add('lastname', TextType::class)
+            ->add('email', TextType::class)
+            ->add('password', TextType::class)
+            ->add('city', TextType::class)
+            ->add('zip', TextType::class)
+            ->add('birthdate', Date::class)
+            ->add('tel', TextType::class);
+    }
 }
