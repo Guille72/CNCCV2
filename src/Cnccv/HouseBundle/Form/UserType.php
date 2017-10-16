5cc497f24f20f5247c9ad86ee88cf58e75c3cfa2
jayce <jayce.grandieres@laposte.net>
2017-10-16 16:09:21 +0200

    CRUD User

---
+++ src/Cnccv/HouseBundle/Form/UserType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Cnccv\HouseBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('firstname')->add('birthdate')->add('address')->add('zip')->add('city')->add('tel')->add('company')->add('siren')->add('created')->add('updated')->add('imageName');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Cnccv\HouseBundle\Entity\User'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'cnccv_housebundle_user';
+    }
+
+
+}
