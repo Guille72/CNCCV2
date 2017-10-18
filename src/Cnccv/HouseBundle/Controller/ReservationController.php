d51388ccc3476063d4db9ff0e1efca64141f4ce3
jayce <jayce.grandieres@laposte.net>
2017-10-18 12:09:40 +0200

    ReservationController

--- src/Cnccv/HouseBundle/Controller/ReservationController.php
+++ src/Cnccv/HouseBundle/Controller/ReservationController.php
@@ -4,7 +4,133 @@ namespace Cnccv\HouseBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Cnccv\HouseBundle\Entity\Reservation;
 
+/**
+ * Reservation Controller
+ *
+ * @Route("reservation")
+ */
 class ReservationController extends Controller
 {
+
+    /**
+     * Lists all reservation entities.
+     *
+     * @Route("/", name="reservation_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $reservations = $em->getRepository('CnccvHouseBundle:Reservation')->findAll();
+
+        return $this->render('reservation/index.html.twig', array(
+            'reservation' => $reservations,
+        ));
+    }
+
+    /**
+     * Creates a new reservation entity.
+     *
+     * @Route("/new", name="reservation_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $reservation = new Reservation();
+        $form = $this->createForm('Cnccv\HouseBundle\Form\ReservationType', $reservation);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($reservation);
+            $em->flush();
+
+            return $this->redirectToRoute('reservation_show', array('id' => $reservation->getId()));
+        }
+
+        return $this->render('user/new.html.twig', array(
+            'reservation' => $reservation,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a reservation entity.
+     *
+     * @Route("/{id}", name="reservation_show")
+     * @Method("GET")
+     */
+    public function showAction(Reservation $reservation)
+    {
+        $deleteForm = $this->createDeleteForm($reservation);
+
+        return $this->render('reservation/show.html.twig', array(
+            'reservation' => $reservation,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing user entity.
+     *
+     * @Route("/{id}/edit", name="reservation_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Reservation $reservation)
+    {
+        $deleteForm = $this->createDeleteForm($reservation);
+        $editForm = $this->createForm('Cnccv\HouseBundle\Form\ReservationType', $reservation);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('reservation_edit', array('id' => $reservation->getId()));
+        }
+
+        return $this->render('reservation/edit.html.twig', array(
+            'reservation' => $reservation,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a reservation entity.
+     *
+     * @Route("/{id}", name="reservation_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Reservation $reservation)
+    {
+        $form = $this->createDeleteForm($reservation);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($reservation);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('reservation_index');
+    }
+
+    /**
+     * Creates a form to delete a reservation entity.
+     *
+     * @param Reservation $reservation The reservation entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Reservation $reservation)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('reservation_delete', array('id' => $reservation->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+            ;
+    }
 }
