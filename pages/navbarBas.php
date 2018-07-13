b41abf78f569b0eb7b6e99906106c66760b6d7f6
guille72 <guillaume@cnccv.fr>
2018-07-13 14:58:12 +0200

    MàJ2 du 13/07/2018

---
+++ pages/navbarBas.php
@@ -0,0 +1,26 @@
+<!-- suite et fin de Navbar -->
+<div class="hide-on-large-only"><br></div>
+<div class="nav-content hide-on-med-and-down displayNScroll" id="cc">
+    <ul class="centreETC tabs-transparent align_ul displayNScroll">
+        <li class="tab li1 li"><a class="active colortext colortext1 waves-green" href="index.php?p=rousseau"><h6>Chez
+                    Rousseau</h6></a></li>
+        <li class="tab li"><a href="index.php?p=champion" class="colortext colortext1 waves-green"><h6>Chez
+                    Champion</h6></a></li>
+        <li class="tab li li3"><a href="index.php?p=painleve" class="colortext colortext1 waves-green"><h6>Chez
+                    Painlevé</h6></a></li>
+    </ul>
+    <br>
+</div>
+<div class="nav-content displayNScroll" id="cc">
+    <ul class="centreETC tabs-transparent hide-on-large-only displayNScroll align_ul">
+        <li class="tab li1 li"><a class="active colortext colortext1 waves-green" href="index.php?p=rousseau" style="font-size: 14px !important;">Chez
+                Rousseau</a></li>
+        <li class="tab li"><a <a href="index.php?p=champion" class="colortext colortext1 waves-green" style="font-size: 14px !important;">Chez
+                Champion</a></li>
+        <li class="tab li li3"><a href="index.php?p=painleve" class="colortext colortext1 waves-green" style="font-size: 14px !important;">Chez
+                Painlevé</a></li>
+    </ul>
+    <br>
+</div>
+
+</nav>
